22aa56e69a8a79346d084f24f0a0d02ed2720744
Loren Gordon <lorengordon@users.noreply.github.com>
2024-12-02 13:42:00 -0800

    Adjust variable validation to account for null contact value

--- variables.tf
+++ variables.tf
@@ -19,38 +19,46 @@ variable "account_alternate_contacts" {
       phone_number  = string
     }))
   })
+
   validation {
     condition = alltrue([
-      for type in var.account_alternate_contacts :
-      length(type.name) >= 1 &&
-      length(type.name) <= 64
+      for contact in var.account_alternate_contacts :
+      length(contact.name) >= 1 &&
+      length(contact.name) <= 64
+      if contact != null
     ])
     error_message = "Invalid value for Name (must be between 1 and 64 characters)"
   }
+
   validation {
     condition = alltrue([
-      for type in var.account_alternate_contacts :
-      length(type.title) >= 1 &&
-      length(type.title) <= 50
+      for contact in var.account_alternate_contacts :
+      length(contact.title) >= 1 &&
+      length(contact.title) <= 50
+      if contact != null
     ])
     error_message = "Invalid value for Title (must be between 1 and 50 characters)"
   }
+
   validation {
     condition = alltrue([
-      for type in var.account_alternate_contacts :
-      length(type.email_address) >= 1 &&
-      length(type.email_address) <= 254 &&
-      can(regex("^[\\s]*[\\w+=.#|!&-]+@[\\w.-]+\\.[\\w]+[\\s]*$", type.email_address))
+      for contact in var.account_alternate_contacts :
+      length(contact.email_address) >= 1 &&
+      length(contact.email_address) <= 254 &&
+      can(regex("^[\\s]*[\\w+=.#|!&-]+@[\\w.-]+\\.[\\w]+[\\s]*$", contact.email_address))
+      if contact != null
     ])
-    error_message = "Invalid value for email address (must be between 1 and 50 characters and correct syntax)"
+    error_message = "Invalid value for email address (must be between 1 and 254 characters and correct syntax)"
   }
+
   validation {
     condition = alltrue([
-      for type in var.account_alternate_contacts :
-      length(type.phone_number) >= 1 &&
-      length(type.phone_number) <= 25 &&
-      can(regex("^[\\s0-9()+-]+$", type.phone_number))
+      for contact in var.account_alternate_contacts :
+      length(contact.phone_number) >= 1 &&
+      length(contact.phone_number) <= 25 &&
+      can(regex("^[\\s0-9()+-]+$", contact.phone_number))
+      if contact != null
     ])
-    error_message = "Invalid value for phone number (must be between 1 and 50 characters and correct syntax)"
+    error_message = "Invalid value for phone number (must be between 1 and 25 characters and correct syntax)"
   }
 }
